2d655ed1403fc23a0230753b6b5744ab3e76e4c9
Zxpro <Zxpro@localhost>
2011-09-10 02:50:15 -0400

    Created a simple ServiceLocator to wrap calls to the IoC container

--- src/FluentLucene/Infrastructure/IServiceLocator.cs
+++ src/FluentLucene/Infrastructure/IServiceLocator.cs
@@ -3,35 +3,22 @@ using System;
 namespace FluentLucene.Infrastructure
 {
     /// <summary>
-    /// Implementation of a very simple service locator
+    /// Represents a simple service locator
     /// </summary>
-    internal class ServiceLocator : IServiceLocator
+    internal interface IServiceLocator
     {
-        private readonly ComponentContainer Container = new ComponentContainer();
-
-        public ServiceLocator()
-        {
-            Container.Instance<IServiceLocator, ServiceLocator>(this);
-        }
-
         /// <summary>
         /// Get a component of the given type
         /// </summary>
         /// <typeparam name="T">The type of the component</typeparam>
         /// <returns>The component of the requested type</returns>
-        public T Get<T>()
-        {
-            return Container.Get<T>();
-        }
+        T Get<T>();
 
         /// <summary>
         /// Get a component of the given type
         /// </summary>
         /// <param name="type">The type of the component</param>
         /// <returns>The component of the requested type</returns>
-        public object Get(Type type)
-        {
-            return Container.Get(type);
-        }
+        object Get(Type type);
     }
 }
